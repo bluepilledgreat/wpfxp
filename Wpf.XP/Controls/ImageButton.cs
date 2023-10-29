f7793fc1bd8c86298a9f80cd85a6a14b382359c6
bluepilledgreat <97983689+bluepilledgreat@users.noreply.github.com>
2023-10-29 01:08:51 +0100

    add code

---
+++ Wpf.XP/Controls/ImageButton.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf.XP.Controls
+{
+    internal class ImageButton : Button
+    {
+        private bool _hover = false;
+        private bool _pressed = false;
+
+        public ImageSource? Image
+        {
+            get => GetValue(ImageProperty) as ImageSource;
+            set => SetValue(ImageProperty, value);
+        }
+
+        public ImageSource? HoverImage
+        {
+            get => GetValue(HoverImageProperty) as ImageSource;
+            set => SetValue(HoverImageProperty, value);
+        }
+
+        public ImageSource? PressImage
+        {
+            get => GetValue(PressImageProperty) as ImageSource;
+            set => SetValue(PressImageProperty, value);
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageButton),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ImagePropertyChanged));
+
+        public static readonly DependencyProperty HoverImageProperty =
+            DependencyProperty.Register("HoverImage", typeof(ImageSource), typeof(ImageButton),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ImagePropertyChanged));
+
+        public static readonly DependencyProperty PressImageProperty =
+            DependencyProperty.Register("PressImage", typeof(ImageSource), typeof(ImageButton),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ImagePropertyChanged));
+
+        private static void ImagePropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            ImageButton? imageButton = source as ImageButton;
+
+            if (imageButton == null)
+                return;
+
+            imageButton.UpdateImage();
+        }
+
+        public ImageButton() : base()
+        {
+            this.MouseEnter += ImageButton_MouseEnter;
+            this.MouseLeave += ImageButton_MouseLeave;
+
+            this.PreviewMouseDown += ImageButton_PreviewMouseDown;
+            this.PreviewMouseUp += ImageButton_PreviewMouseUp;
+
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            // TODO: create image automatically
+            Image? image = this.Content as Image;
+
+            if (image == null)
+                return;
+
+            if (_pressed && this.PressImage != null)
+            {
+                image.Source = this.PressImage;
+            }
+            else if (_hover && this.HoverImage != null)
+            {
+                image.Source = this.HoverImage;
+            }
+            else
+            {
+                image.Source = this.Image;
+            }
+        }
+
+        protected override void OnContentChanged(object oldValue, object newValue)
+        {
+            UpdateImage();
+
+            base.OnContentChanged(oldValue, newValue);
+        }
+
+        private void ImageButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            _hover = true;
+            UpdateImage();
+        }
+
+        private void ImageButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            _hover = false;
+            UpdateImage();
+        }
+
+        private void ImageButton_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _pressed = true;
+            UpdateImage();
+        }
+
+        private void ImageButton_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _pressed = false;
+            UpdateImage();
+        }
+    }
+}
---
+++ Wpf.XP/Controls/WindowFrame.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+
+namespace Wpf.XP.Controls
+{
+    /// <summary>
+    /// Windows XP Window Frame
+    /// </summary>
+    public class WindowFrame : ContentControl
+    {
+        public string Title
+        {
+            get => GetValue(TitleProperty) as string ?? "";
+            set => SetValue(TitleProperty, value);
+        }
+
+        public ImageSource? Icon
+        {
+            get => GetValue(IconProperty) as ImageSource;
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(WindowFrame),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(WindowFrame),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        private Window _window = null!;
+
+        private TextBlock _title = null!;
+
+        private Image _titleBarLeft = null!;
+        private Image _titleBarMiddle = null!;
+        private Image _titleBarRight = null!;
+
+        private Image _sideLeft = null!;
+        private Image _sideRight = null!;
+
+        private Image _bottomLeft = null!;
+        private Image _bottomMiddle = null!;
+        private Image _bottomRight = null!;
+
+        private ImageButton _minimizeButton = null!;
+        private ImageButton _maximizeButton = null!;
+        private ImageButton _restoreButton = null!;
+        private ImageButton _closeButton = null!;
+
+        public WindowFrame() : base()
+        {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            bool designer = DesignerProperties.GetIsInDesignMode(this);
+
+            // designer does not like any of this
+            if (designer)
+                return;
+
+            _window = Window.GetWindow(this);
+
+            _title = FindChild<TextBlock>(this, "Title")!;
+
+            _titleBarLeft = FindChild<Image>(this, "TitleBarLeft")!;
+            _titleBarMiddle = FindChild<Image>(this, "TitleBarMiddle")!;
+            _titleBarRight = FindChild<Image>(this, "TitleBarRight")!;
+
+            _sideLeft = FindChild<Image>(this, "SideLeft")!;
+            _sideRight = FindChild<Image>(this, "SideRight")!;
+
+            _bottomLeft = FindChild<Image>(this, "BottomLeft")!;
+            _bottomMiddle = FindChild<Image>(this, "BottomMiddle")!;
+            _bottomRight = FindChild<Image>(this, "BottomRight")!;
+
+            _minimizeButton = FindChild<ImageButton>(this, "MinimizeButton")!;
+            _maximizeButton = FindChild<ImageButton>(this, "MaximizeButton")!;
+            _restoreButton = FindChild<ImageButton>(this, "RestoreButton")!;
+            _closeButton = FindChild<ImageButton>(this, "CloseButton")!;
+
+            HookWindowProc();
+            _window.StateChanged += Window_StateChanged;
+
+            _window.Activated += Window_Activated;
+            _window.Deactivated += Window_Deactivated;
+
+            _minimizeButton.Click += MinimizeButton_Click;
+            _maximizeButton.Click += MaximizeButton_Click;
+            _restoreButton.Click += RestoreButton_Click;
+            _closeButton.Click += CloseButton_Click;
+
+            var windowChrome = new WindowChrome
+            {
+                ResizeBorderThickness = new Thickness(4),
+                CaptionHeight = 30,
+                GlassFrameThickness = new Thickness(4)
+            };
+
+            WindowChrome.SetWindowChrome(_window, windowChrome);
+        }
+
+        // to fix maximize - https://stackoverflow.com/a/46465322
+        private void HookWindowProc()
+        {
+            IntPtr handle = new WindowInteropHelper(_window).Handle;
+            HwndSource.FromHwnd(handle)?.AddHook(WindowProc);
+        }
+
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    Native.WmGetMinMaxInfo(hwnd, lParam, (int)MinWidth, (int)MinHeight);
+                    handled = true;
+                    break;
+            }
+
+            return (IntPtr)0;
+        }
+
+        private ImageSource CreateImageSource(string uri)
+        {
+            return new BitmapImage(new Uri("pack://application:,,,/Wpf.XP;component/Resources/Frame/" + uri + ".png"));
+        }
+
+        private DropShadowEffect CreateNewDropShadowEffect(DropShadowEffect effect, double newOpacity)
+        {
+            DropShadowEffect newEffect = new DropShadowEffect();
+
+            newEffect.BlurRadius = effect.BlurRadius;
+            newEffect.Direction = effect.Direction;
+            newEffect.Opacity = newOpacity;
+            newEffect.ShadowDepth = effect.ShadowDepth;
+            newEffect.Color = effect.Color;
+
+            return newEffect;
+        }
+
+        private void ChangeActive(bool isActive)
+        {
+            string suffix = isActive ? "" : "Inactive";
+
+            _titleBarLeft.Source = CreateImageSource("TitleLeft" + suffix);
+            _titleBarMiddle.Source = CreateImageSource("TitleMiddle" + suffix);
+            _titleBarRight.Source = CreateImageSource("TitleRight" + suffix);
+
+            _sideLeft.Source = CreateImageSource("SideLeft" + suffix);
+            _sideRight.Source = CreateImageSource("SideRight" + suffix);
+
+            _bottomLeft.Source = CreateImageSource("BottomLeft" + suffix);
+            _bottomMiddle.Source = CreateImageSource("BottomMiddle" + suffix);
+            _bottomRight.Source = CreateImageSource("BottomRight" + suffix);
+
+            _minimizeButton.Image = CreateImageSource("MinimizeButton" + suffix);
+            _maximizeButton.Image = CreateImageSource("MaximizeButton" + suffix);
+            _restoreButton.Image = CreateImageSource("RestoreButton" + suffix);
+            _closeButton.Image = CreateImageSource("CloseButton" + suffix);
+
+            Color textColor = new Color();
+
+            textColor.R = (byte)(isActive ? 255 : 216);
+            textColor.G = (byte)(isActive ? 255 : 228);
+            textColor.B = (byte)(isActive ? 255 : 248);
+            textColor.A = 255;
+
+            _title.Foreground = new SolidColorBrush(textColor);
+
+            // effect is read only so we have to create a new one every time
+            var effect = CreateNewDropShadowEffect((DropShadowEffect)_title.Effect, isActive ? 1d : 0d);
+            _title.Effect = effect;
+        }
+
+        private void Window_Activated(object? sender, EventArgs e)
+        {
+            ChangeActive(true);
+        }
+
+        private void Window_Deactivated(object? sender, EventArgs e)
+        {
+            ChangeActive(false);
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            _window.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            _window.WindowState = WindowState.Maximized;
+        }
+
+        private void RestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            _window.WindowState = WindowState.Normal;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            _window.Close();
+        }
+
+        private void Window_StateChanged(object? sender, EventArgs e)
+        {
+            if (_window.WindowState == WindowState.Maximized)
+            {
+                _maximizeButton.Visibility = Visibility.Collapsed;
+                _restoreButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                _maximizeButton.Visibility = Visibility.Visible;
+                _restoreButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// Finds a Child of a given item in the visual tree. <br/>
+        /// https://stackoverflow.com/a/1759923
+        /// </summary>
+        /// <param name="parent">A direct parent of the queried item.</param>
+        /// <typeparam name="T">The type of the queried item.</typeparam>
+        /// <param name="childName">x:Name or Name of child. </param>
+        /// <returns>The first parent item that matches the submitted type parameter. 
+        /// If not matching item can be found, 
+        /// a null parent is being returned.</returns>
+        public static T? FindChild<T>(DependencyObject parent, string childName)
+           where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T? foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T? childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+        static WindowFrame()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowFrame), new FrameworkPropertyMetadata(typeof(WindowFrame)));
+        }
+    }
+}
